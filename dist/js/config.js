1015a886233699c10dda5c1d9f74a9e030a972b5
mat <mat@pallando.emi.u-bordeaux1.fr>
2014-02-12 13:03:20 +0100

    Nope.avi

--- dist/js/config.js
+++ dist/js/config.js
@@ -2,10 +2,21 @@
 
 var myNeighTool = angular.module('myNeighTool', []);
      
-    myNeighTool.controller('SiteConstantsListCtrl', function ($scope) {
-    $scope.constants = [
-    {'siteName': 'MyNeighTool',
-    'siteSlogan': 'Pretez avec vos voisin',
-    'siteMainPage': 'index.html'}
-    ];
-    });
+	myNeighTool.controller('SiteConstantsListCtrl', function ($scope) {
+    $scope.constants = [{
+    	'siteName': 'MyNeighTool',
+    	'siteSlogan': 'Pretez avec vos voisin',
+    	'siteMainPage': 'index.html'
+    }];
+    
+    $scope.botmenu = [
+	{
+    	'menuLink' = 'index.html',
+    	'menuName' = 'Index'
+    },
+    {
+    	'menuLink' = 'contact.html',
+    	'menuName' = 'Contact'
+    }];
+    
+});
